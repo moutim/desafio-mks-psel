a331f3b3d7da23fb515bf372f623890d61d41cf9
Vitor Moutim <moutimg@gmail.com>
2024-04-30 12:00:18 -0300

    fix: remove arquivos desnecessarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { join } from 'path';
@@ -25,7 +23,5 @@ import { MoviesModule } from './modules/movies.module';
     }),
     MoviesModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
