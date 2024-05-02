75e338664738f6b36b4c3c09f7841e0ef2c964d9
Vitor Moutim <moutimg@gmail.com>
2024-05-02 13:07:40 -0300

    feat: cria CRUD para a tabela users

--- src/modules/users.module.ts
+++ src/modules/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from '../services/users/users.service';
 import { UsersController } from '../controllers/users/users.controller';
+import { User } from 'src/entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService],
 })
--- src/services/users/users.service.ts
+++ src/services/users/users.service.ts
@@ -1,26 +1,97 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/entities/user.entity';
+import { Repository } from 'typeorm';
+import { hash } from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const checkUser = await this.userRepository.findOne({
+      where: { email: createUserDto.email },
+    });
+
+    if (checkUser) {
+      throw new ConflictException('Usuário já cadastrado com este email.');
+    }
+
+    const encodedPassword = await hash(createUserDto.password, 8);
+
+    const user = {
+      name: createUserDto.name,
+      email: createUserDto.email,
+      password: encodedPassword,
+    };
+
+    const userCreated = await this.userRepository.save(user);
+
+    delete userCreated.password;
+    return userCreated;
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll() {
+    const users = await this.userRepository.find();
+
+    if (!users) {
+      throw new NotFoundException('Não há usuários cadastrados.');
+    }
+
+    return users;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    const user = await this.userRepository.findOne({ where: { id } });
+
+    if (!user) {
+      throw new NotFoundException(`Usuário com id ${id} não encontrado.`);
+    }
+
+    delete user.password;
+    return user;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, updateInfo: UpdateUserDto) {
+    const user = await this.userRepository.findOne({ where: { id } });
+
+    if (!user) {
+      throw new NotFoundException(`Usuário com id ${id} não encontrado.`);
+    }
+
+    if (updateInfo.email) {
+      const checkEmail = await this.userRepository.findOne({
+        where: { email: updateInfo.email },
+      });
+
+      if (checkEmail && checkEmail.id !== user.id) {
+        throw new ConflictException('Usuário já cadastrado com este email.');
+      }
+    }
+
+    Object.assign(user, updateInfo);
+
+    delete user.password;
+    return await this.userRepository.save(user);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number) {
+    const user = await this.userRepository.findOne({ where: { id } });
+
+    if (!user) {
+      throw new NotFoundException(`Usuário com id ${id} não encontrado.`);
+    }
+
+    await this.userRepository.delete(user);
+
+    return { message: 'Usuário deletado com sucesso.' };
   }
 }
